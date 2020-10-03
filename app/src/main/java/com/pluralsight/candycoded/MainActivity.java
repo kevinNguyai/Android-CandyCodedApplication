e640ad2eda6f9444092577ec9350ee5a9b2048fc
Kevin <wainainakelvine@gmail.com>
2020-10-03 13:04:41 +0300

    added the store information activity intent

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -95,5 +96,17 @@ public class MainActivity extends AppCompatActivity {
 
             db.insert(CandyEntry.TABLE_NAME, null, values);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.info:
+                Intent infoIntent = new Intent(MainActivity.this,InfoActivity.class);
+                startActivity(infoIntent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
